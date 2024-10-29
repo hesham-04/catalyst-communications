687cba19c6bce9610e1f335450b44d5de103f652
hesham <hesham.fractured@gmail.com>
2024-10-29 17:33:51 +0500

    Project: File Structure

---
+++ root/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.static import serve
+
+from root.settings import MEDIA_ROOT, STATIC_ROOT
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('src.web.urls')),
+]
+
+""" STATIC AND MEDIA FILES ----------------------------------------------------------------------------------------- """
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': STATIC_ROOT}),
+]
