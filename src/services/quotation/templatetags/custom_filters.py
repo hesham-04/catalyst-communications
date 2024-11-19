11f35a729b9e2c1baa2350092b43ca68eeb2637b
hesham <hesham.fractured@gmail.com>
2024-11-19 18:17:51 +0500

    Services: New Tax Models

--- src/services/quotation/templatetags/custom_filters.py
+++ src/services/quotation/templatetags/custom_filters.py
@@ -20,3 +20,13 @@ def capitalize_and_replace(value):
     value = value.replace("euro, zero cents", "rupees")
 
     return value
+
+
+@register.filter
+def apply_tax(total_amount, percent_tax):
+    try:
+        tax_amount = (total_amount * percent_tax) / 100
+        total_with_tax = total_amount + tax_amount
+        return round(total_with_tax, 2)  # round to 2 decimal places if needed
+    except (TypeError, ValueError):
+        return total_amount  # return the original amount if there's an error
