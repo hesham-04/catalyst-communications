c3faea2670cd384653db031a5063f022a5dbd263
hesham <hesham.fractured@gmail.com>
2024-12-19 22:10:24 +0500

    Dashboard

--- src/web/dashboard/views.py
+++ src/web/dashboard/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Sum
 from django.views.generic import TemplateView
+from django.db.models import Sum, Q
 
 from src.services.assets.models import CashInHand, AccountBalance
 from src.services.customer.models import Customer
+from src.services.expense.models import Expense
 from src.services.invoice.models import Invoice
 from src.services.loan.models import Loan
 from src.services.transaction.models import Ledger
@@ -12,7 +13,7 @@ from src.web.dashboard.utils import get_monthly_income_expense
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
-    template_name = 'dashboard/dashboard.html'
+    template_name = "dashboard/dashboard.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -20,16 +21,23 @@ class HomeView(LoginRequiredMixin, TemplateView):
 
         cash_in_hand = CashInHand.objects.first()
         cash_in_hand_balance = cash_in_hand.balance if cash_in_hand else 0
-        account_balance_balance = AccountBalance.objects.aggregate(total=Sum('balance'))['total'] or 0
+        account_balance_balance = (
+            AccountBalance.objects.aggregate(total=Sum("balance"))["total"] or 0
+        )
 
-        context['cash_in_hand'] = cash_in_hand_balance
-        context['income'] = income
-        context['expense'] = expense
-        context['account_balance'] = account_balance_balance
-        context['total_balance'] = cash_in_hand_balance + account_balance_balance
-        context['customer_count'] = Customer.objects.count()
-        context['payable'] = Loan.calculate_total_unpaid_amount()
-        context['receivables'] = Invoice.calculate_total_receivables()
-        context['top_vendors'] = Vendor.objects.order_by('-total_expense')[:8]
-        context['transactions'] = Ledger.objects.order_by('-created_at')[:8]
+        context["cash_in_hand"] = cash_in_hand_balance
+        context["income"] = income
+        context["expense"] = expense
+        context["account_balance"] = account_balance_balance
+        context["total_balance"] = cash_in_hand_balance + account_balance_balance
+        context["customer_count"] = Customer.objects.count()
+        context["payable"] = Loan.calculate_total_unpaid_amount()
+        context["receivables"] = Invoice.calculate_total_receivables()
+        context["top_vendors"] = Vendor.objects.annotate(
+            total_paid_expense=Sum(
+                "expenses__amount",
+                filter=Q(expenses__payment_status=Expense.PaymentStatus.PAID),
+            )
+        ).order_by("-total_paid_expense")[:8]
+        context["transactions"] = Ledger.objects.order_by("-created_at")[:8]
         return context
